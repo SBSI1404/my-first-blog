d4ff3927a9fc6e9b6698c6bf2b05d94ae7914205
SBSI1404 <m_kawasaki@sbs-infosys.co.jp>
2021-08-20 15:33:20 +0900

    My Django Girls app, 4th commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,10 +132,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS =[
+#STATICFILES_DIRS =[
 #   os.path.join(BASE_DIR, 'carzone/static'),
-   os.path.join(BASE_DIR, 'static'),
-]
+#   os.path.join(BASE_DIR, 'static'),
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
